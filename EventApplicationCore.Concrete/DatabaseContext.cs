23635ec24bd8b23625f6fa3f8f590a024bf84c2c
bhushan91 <bhushanpatki@yahoo.com>
2018-02-18 23:36:48 +0530

    Add Project file

---
+++ EventApplicationCore.Concrete/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using EventApplicationCore.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventApplicationCore.Concrete
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Registration> Registration { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<States> States { get; set; }
+        public DbSet<Roles> Roles { get; set; }
+        public DbSet<Venue> Venue { get; set; }
+       
+       
+        public DbSet<Dishtypes> Dishtypes { get; set; }
+       
+       
+        public DbSet<BookingDetails> BookingDetails { get; set; }
+        public DbSet<BookingVenue> BookingVenue { get; set; }
+        public DbSet<Event> Event { get; set; }
+        
+    }
+}
